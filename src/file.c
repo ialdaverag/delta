88befd36fab2154d54704a536d90192cfd674c02
Ivan <elite-a64@outlook.com>
2025-03-25 21:22:47 -0600

    FILE: Verifica si el archivo se pudo abrir

--- src/file.c
+++ src/file.c
@@ -5,6 +5,14 @@ void File_init(File* file, const char* filename) {
 
     FILE* fp = fopen(filename, "rb");
 
+    // Verifica si el archivo se pudo abrir
+    if (fp == NULL) {
+        fprintf(stderr, "Error: No se puede abrir el archivo '%s'\n", filename);
+        File_free(file);
+
+        return;
+    }
+
     fseek(fp, 0, SEEK_END);
     file->length = ftell(fp);
     fseek(fp, 0, SEEK_SET);
